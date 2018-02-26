efaa1b4a027f939b544ac1b0bde18352309656fb
Crpaxton4 <paxtoncr@miamioh.edu>
2018-02-26 15:59:10 -0500

    Added som dummy lines

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Main");
+		int x = 5;
+		x++;
+		System.out.println(x);
 	}
 
 }
