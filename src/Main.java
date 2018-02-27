52d93828cd9c23e95f884008e70a31e1c46a817e
Crpaxton4 <paxtoncr@miamioh.edu>
2018-02-27 14:45:22 -0500

    addes some methods and a test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
 
+/**
+ * @author Chris
+ *
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Main");
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @return greater of a and b
+	 */
+	public int max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	
+	/**
+	 * @param n
+	 * @return the nth number in the Fibonacci sequence
+	 */
+	public int fib(int n) {
+		int[] memo = new int [n+1];
+		memo[0] = 0;
+		memo[1] = 1;
+		
+		for(int i = 2; i < n; i++) {
+			memo[i] = memo[i-1] + memo[i-2];
+		}
+		return memo[n-1];
 	}
 
 }
--- test/TestMain.java
+++ test/TestMain.java
@@ -4,9 +4,20 @@ import org.junit.Test;
 
 public class TestMain {
 
+	
 	@Test
 	public void test() {
 		assertTrue(true);
 	}
 
+	@Test
+	public void testFib() {
+		Main m = new Main();
+		
+		int n = 4;
+		
+		int f = m.fib(n);
+		
+		assertEquals(f, 3)
+	}
 }
