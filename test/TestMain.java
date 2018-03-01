f8eeba6c5ac95e638e232f514839e9d22d9a998d
Crpaxton4 <paxtoncr@miamioh.edu>
2018-02-28 22:37:14 -0500

    Server Setup

--- test/TestMain.java
+++ test/TestMain.java
@@ -3,9 +3,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestMain {
-
+	
+	Main m;
+	
+	@Before
+	public void setup() {
+		m = new Main();
+	}
 	@Test
 	public void testMax() {
-		
+		assertEquals(15, m.max(3, 15));
 	}
 }
