990e3ae62f1df132af73b1a3d11f2de7b2379354
Crpaxton4 <paxtoncr@miamioh.edu>
2018-03-01 21:36:50 -0500

    Implemented Methods

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,35 @@
 public class Main {
 	
 	public int fib(int n) {
-		return -1;
+		
+		if(n == 0) {
+			return 0;
+		}
+		
+		int[] memo = new int[n+1];
+		memo[0] = 0;
+		memo[1] = 1;
+		
+		for(int i = 2; i <=n; i++) {
+			memo[i] = memo[i-1] + memo[i-2];
+		}
+		
+		return memo[n];
 	}
 	
-	public String reverseString(String s) {
-		return null;
+	public String reverseString(String s) {		
+		StringBuilder sb = new StringBuilder();
+		sb.append(s);
+		return sb.reverse().toString();
 	}
 	
 	public int countPositive(int[] arr) {
-		return -1;
+		int count = 0;
+		for(int i : arr) {
+			if(i > 0) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
--- src/TestMain.java
+++ src/TestMain.java
@@ -14,15 +14,10 @@ public class TestMain {
 	
 	@Test
 	public void testFib() {
-		
-		assertEquals(0, fib(0));
-		
+		assertEquals(0, m.fib(0));
 		assertEquals(1, m.fib(1));
-		
 		assertEquals(3, m.fib(4));
-		
 		assertEquals(89, m.fib(11));
-		
 		assertEquals(6765, m.fib(20));
 	}
 	
@@ -30,10 +25,8 @@ public class TestMain {
 	public void testReverseString() {
 		String s1 = "Reverse";
 		assertTrue(m.reverseString(s1).equals("esreveR"));
-		
 		String s2 = "";
 		assertTrue(m.reverseString(s2).equals(""));
-		
 		String s3 = "aaaa";
 		assertTrue(m.reverseString(s3).equals("aaaa"));
 	}
@@ -42,17 +35,12 @@ public class TestMain {
 	public void testCountPositive() {
 		int[] a1 = {1, 2, 3, -1, -5, 4, -1, 6, 6};
 		assertEquals(6, m.countPositive(a1));
-		
 		int[] a2 = new int[0];
 		assertEquals(0, m.countPositive(a2));
-		
 		int[] a3 = {-1, -2, -1, -6, -5, -4, -9, -3};
 		assertEquals(0, m.countPositive(a3));
-		
 		int[] a4 = {1, 2, 3, 4, 5, 1, 6};
 		assertEquals(7, m.countPositive(a4));
-		
-		
 	}
 
 }
